d7196a9275c0c0c8429242cd966f29ab37eecf7d
Oliver Booth <me@olivr.me>
2022-03-22 22:14:26 +0000

    Add NormalizeClientAsync
    This allows plugins to normalize various SnowflakeObjects so that the underlying client corresponds to a desired value

--- BrackeysBot.API/Extensions/DiscordMessageExtensions.cs
+++ BrackeysBot.API/Extensions/DiscordMessageExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using DisCatSharp;
 using DisCatSharp.Entities;
 
 namespace BrackeysBot.API.Extensions;
@@ -15,5 +17,28 @@ public static class DiscordMessageExtensions
     public static Task AcknowledgeAsync(this DiscordMessage message)
     {
         return message.CreateReactionAsync(DiscordEmoji.FromUnicode("✅"));
+    }
+
+    /// <summary>
+    ///     Normalizes a <see cref="DiscordMessage" /> so that the internal client is assured to be a specified value.
+    /// </summary>
+    /// <param name="message">The <see cref="DiscordMessage" /> to normalize.</param>
+    /// <param name="client">The target client.</param>
+    /// <returns>
+    ///     A <see cref="DiscordMessage" /> whose public values will match <paramref name="message" />, but whose internal client
+    ///     is <paramref name="client" />.
+    /// </returns>
+    /// <exception cref="ArgumentNullException">
+    ///     <para><paramref name="message" /> is <see langword="null" /></para>
+    ///     -or-
+    ///     <para><paramref name="client" /> is <see langword="null" /></para>
+    /// </exception>
+    public static async Task<DiscordMessage> NormalizeClientAsync(this DiscordMessage message, DiscordClient client)
+    {
+        if (message is null) throw new ArgumentNullException(nameof(message));
+        if (client is null) throw new ArgumentNullException(nameof(client));
+
+        DiscordChannel channel = await message.Channel.NormalizeClientAsync(client);
+        return await channel.GetMessageAsync(message.Id);
     }
 }
---
+++ BrackeysBot.API/Extensions/DiscordUserExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using DisCatSharp;
+using DisCatSharp.Entities;
+
+namespace BrackeysBot.API.Extensions;
+
+/// <summary>
+///     Extension methods for <see cref="DiscordUser" />.
+/// </summary>
+public static class DiscordUserExtensions
+{
+    /// <summary>
+    ///     Normalizes a <see cref="DiscordUser" /> so that the internal client is assured to be a specified value.
+    /// </summary>
+    /// <param name="user">The <see cref="DiscordUser" /> to normalize.</param>
+    /// <param name="client">The target client.</param>
+    /// <returns>
+    ///     A <see cref="DiscordUser" /> whose public values will match <paramref name="user" />, but whose internal client
+    ///     is <paramref name="client" />.
+    /// </returns>
+    /// <exception cref="ArgumentNullException">
+    ///     <para><paramref name="user" /> is <see langword="null" /></para>
+    ///     -or-
+    ///     <para><paramref name="client" /> is <see langword="null" /></para>
+    /// </exception>
+    public static Task<DiscordUser> NormalizeClientAsync(this DiscordUser user, DiscordClient client)
+    {
+        if (user is null) throw new ArgumentNullException(nameof(user));
+        if (client is null) throw new ArgumentNullException(nameof(client));
+
+        return client.GetUserAsync(user.Id);
+    }
+}
